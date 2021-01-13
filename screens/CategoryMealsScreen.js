8c67da8402270367e8d2b881140660bed14f077a
kimjinsoo <roorooro9933@gmail.com>
2021-01-13 17:50:01 +0900

    setting dynamic navigation options

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -2,21 +2,15 @@ import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
 import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
 const CategoryMealsScreen = (props) => {
-  // 여기서는 parameter로 넘겨준 것을 받아야겠지.
-  // key값을 적어주면은 그거에 대한 value를 받을 수 있다.
-  // 뭐 굉장히 자연스러운 흐름이라고 생각된다.
-
   const catId = props.navigation.getParam("categoryId");
 
-  // 뭐 id를 받아가지고 그 id에 해당하는 category를 다시 찾는건데.
-  // 글쎄다 뭐 두번 일을 하는 것 같지만. 뭐 이렇게 로직을 짤 수도 있다 정도.
   const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
   return (
     <View style={styles.screen}>
       <Text>This is Category Meals Screen</Text>
-      {/* 여기서 뭐 찍어봤다. */}
       <Text>{selectedCategory.title}</Text>
       <Button
         title="Go to Details"
@@ -32,6 +26,32 @@ const CategoryMealsScreen = (props) => {
       />
     </View>
   );
+};
+
+// 일단 여기서 문제는 navigationOptions를 Object로 만들 수 없었다.
+// 왜냐하면은 catId를 이용해서 headerTitle을 적어주고 싶었는데
+// 그 headerTitle은 동적으로 변하기 때문이였다.
+// 동적인 것을 이용해서 navigationOptions을 이용하고 싶을떄는
+// 이렇게 함수를 만들어줘서 사용을 하면 된다는 것임.
+// 뭐 당연히 정적인것보다 동적인게 더 많이 쓰일테니까
+// Object로 하는 것 보다 이렇게 function으로 만들어주는 것을
+// 더 많이 사용할 것 같다.
+CategoryMealsScreen.navigationOptions = (navigationData) => {
+  // 뭐 아래 있는건 test이고 이렇게 접근을 하면 된다는 것을 보여준다 저옫
+  // 생각을 하면 되겠지.
+  // console.log(navigationData.navigation.getParam("categoryId"));
+
+  const catId = navigationData.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
+  // 결국엔 이것도 Object를 return하게 되어 있다.
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Colors.primaryColor,
+    },
+    headerTintColor: "white",
+  };
 };
 
 const styles = StyleSheet.create({
