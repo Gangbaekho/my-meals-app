3a678fa0b7fd1e969f0f27ced66916ccd0e40270
kimjinsoo <roorooro9933@gmail.com>
2021-01-13 17:39:24 +0900

    padding and reading params upon navigation

--- screens/CatagoriesScreen.js
+++ screens/CatagoriesScreen.js
@@ -12,18 +12,18 @@ import Colors from "../constants/Colors";
 const CategoriesScreen = (props) => {
   const renderGridItem = (itemData) => {
     return (
-      // 원래 View 자체는 Touchable 한게 아니라는 거임.
-      // web에서는 원래 모든 Element가 Touchable인것에
-      // 반해 여기서는 Touchable 계열을 써줘야지 터치가 가능하다 정도
-      // 다시한번 기억해두면 된다.
-      // 아 그리고 props라는 것을 쓰기 위해서 이 안으로 옮겼다.
-      // 하지만 뭐 이럴바에는 그냥 하나 Component를 만들지 그러냐.
       <TouchableOpacity
         style={styles.gridItem}
         onPress={() => {
-          // 뭐 일단은 누르면은 그거에 맞는 Detail에 가는게 원래 맞지만은
-          // 일단은 여기로 가게 만들자.
-          props.navigation.navigate({ routeName: "CategoryMeals" });
+          // 여기서 알아야 할 것은 navigate해서 CategoryMeals로 가긴 갈 건데
+          // 특정한 parameters를 전달 할 수 있다는 거임. 저렇게 써준다음에
+          // 넘기고 싶은 key / value를 넘겨주면 된다는 것이다.
+          props.navigation.navigate({
+            routeName: "CategoryMeals",
+            params: {
+              categoryId: itemData.item.id,
+            },
+          });
         }}
       >
         <View>
@@ -43,23 +43,11 @@ const CategoriesScreen = (props) => {
   );
 };
 
-// CategoriesScreen은 Javascript에서 Function이고
-// Javascript에서 Function은 곧 Object 이니까
-// 이렇게 단순하게 새로운 property를 만들 수 있다는 것이다.
-// 이걸 이용하면은 뭐 여러개 바꿀 수 있다는 것 같네.
-// 이러한 정보는 navigation이라는 것이 이용하는 것이겠고.
-// 뭐 그렇게 단순하게 이해를 하면 될 듯 하다.
-
-// Document가서 또 다른 properties에 대해서
-// 알아보고 적용을 하면 더 좋을 것이다.
-// 당연히 여기서 다 알려줄 수는 없는 거겠찌.
 CategoriesScreen.navigationOptions = {
   headerTitle: "Meal Categories!!!",
   headerStyle: {
     backgroundColor: Colors.primaryColor,
   },
-  // TintColor라는거는 그냥 Color랑 같은 거임.
-  // 글자색이 바뀌는 거임.
   headerTintColor: "white",
 };
 
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+import { CATEGORIES } from "../data/dummy-data";
+
 const CategoryMealsScreen = (props) => {
+  // 여기서는 parameter로 넘겨준 것을 받아야겠지.
+  // key값을 적어주면은 그거에 대한 value를 받을 수 있다.
+  // 뭐 굉장히 자연스러운 흐름이라고 생각된다.
+
+  const catId = props.navigation.getParam("categoryId");
+
+  // 뭐 id를 받아가지고 그 id에 해당하는 category를 다시 찾는건데.
+  // 글쎄다 뭐 두번 일을 하는 것 같지만. 뭐 이렇게 로직을 짤 수도 있다 정도.
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
   return (
     <View style={styles.screen}>
       <Text>This is Category Meals Screen</Text>
+      {/* 여기서 뭐 찍어봤다. */}
+      <Text>{selectedCategory.title}</Text>
       <Button
         title="Go to Details"
         onPress={() => {
