0a46f026de34b5e58ddf96135cde44e868410e78
kimjinsoo <roorooro9933@gmail.com>
2021-01-13 16:41:24 +0900

    pushing, popping and replacing

--- screens/CatagoriesScreen.js
+++ screens/CatagoriesScreen.js
@@ -2,30 +2,20 @@ import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
 const CategoriesScreen = (props) => {
-  // 일단은 CategorisScreen 같은 경우는
-  // MealsNavigator에 포함되어 있기 때문에
-  // 특별한 props를 가진다. 그렇기 떄문에 여기서 이걸로 체크를
-  // 해보겠다는 것임.
-  // 뭐 여러가지가 있는데, 그것에 대해서는 그냥 Docs를 보고
-  // 여기서는 필요한 것만 빨리빨리 캐치를 하자.
-  // console.log(props);
   return (
     <View style={styles.screen}>
       <Text>This is Categories Screen</Text>
       <Button
         title="Go to Meals!"
         onPress={() => {
-          // navigate라는 것이 사실 핵심이다.
-          // routeName에다가 Navigator에 정의한 것을 넣어주면은
-          // 거길로 이동한다는 말인 듯 함.
-          // props.navigation.navigate('CategoryMeals') 이렇게 바로 줘도 되긴 함.
-          // 이걸 Object로 받는다는 것은 뭐 다양한 옵션을 줄 수도 있다는 뜻 같은데
-          // 별다른 옵션을 주지 않는다면 뭐 저렇게 해도 상관 없는 듯 함.
-          // 저번에는 if 랑 뭐 그런걸로 바꿧다면 이제는 navigation으로 바꾸니까
-          // 좀 더 잘 만든 버전이라고 할 수 있겠음.
-          // 근데 주의해야 할 것은 우리가 stack navigation을 import 했으니까
-          // 이게 계속 쌓인다는 개념을 가지고 가야 한다. 그러니까 push를 한거랑 같은 거임.
           props.navigation.navigate({ routeName: "CategoryMeals" });
+          // 사실 다른 페이지로 넘어가는 다른 방법이 있긴 함.
+          // props.navigation.push를 써도 되긴 함.
+          // props.navigation.replace('CategoryMeals');
+          // 이렇게 해도 넘어가긴 하는데 문제는, Page 전환 애니메이션이 적용이
+          // 안될 뿐 더러, Stack에 쌓이지 않기 때문에, 뒤돌아가기 버튼이
+          // 활성화 되지 않는다는 특징이 있다. 그냥 stack에 쌓이지 않고 이동하는
+          // 뭐 그런거라고 간단하게 생각을 하면 될 듯.
         }}
       />
     </View>
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const MealDetailScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>This is Meal Detail Screen</Text>
+      <Button
+        title="Go back to Categories"
+        onPress={() => {
+          // 그냥 popToTop을 쓰면은 가장 처음에 나오는
+          // Screen으로 돌아간다 뭐 그런 말임.
+          // 그냥 처음 화면? 으로 돌아간다 뭐 그런거.
+          // 여기서 그냥 pop을 쓴다면은 CategoryMealsScreen 으로 가겠지만
+          // 여기서 내가 원하는 것은 CategoriesScreen으로 가는 것 이기 떄문에
+          // pop 대신에 popToTop을 썻다는 것 정도? 알아두면 되겠다.
+          props.navigation.popToTop();
+        }}
+      />
     </View>
   );
 };
