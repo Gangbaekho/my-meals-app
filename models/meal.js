86e03d94cb7b124cba21fb9900cb83a4638d0156
kimjinsoo <roorooro9933@gmail.com>
2021-01-14 15:26:36 +0900

    adding meal models and data

---
+++ models/meal.js
@@ -0,0 +1,33 @@
+// 글쎄다. 너무 instance 변수가 많은데?
+// 그렇게 바람직한 class는 아닌 듯 하다.
+// 뭐 일단 연습이니까 그려려니 한다.
+class Meal {
+  constructor(
+    id,
+    categoryIds,
+    title,
+    affordability,
+    complexity,
+    imageUrl,
+    duration,
+    steps,
+    isGlutenFree,
+    isVegan,
+    isVegetarian,
+    isLactoseFree
+  ) {
+    this.id = id;
+    this.categoryIds = categoryIds;
+    this.title = title;
+    this.affordability = affordability;
+    this.complexity = complexity;
+    this, (imageUrl = imageUrl);
+    this.duration = duration;
+    this.steps = steps;
+    this.isGlutenFree = isGlutenFree;
+    this.isVegan = isVegan;
+    this.isVegetarian = isVegetarian;
+    this.isLactoseFree = isLactoseFree;
+  }
+}
+export default Meal;
