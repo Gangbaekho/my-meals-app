eb0f8328ef26c96b2007bd68dcb98ef8ab1a82fd
kimjinsoo <roorooro9933@gmail.com>
2021-01-18 12:36:30 +0900

    configuring the drawer

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -4,10 +4,8 @@ import { createAppContainer } from "react-navigation";
 import { Ionicons } from "@expo/vector-icons";
 import { Platform } from "react-native";
 
-// Navigatoer
 import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
 import { createBottomTabNavigator } from "react-navigation-tabs";
-// 이번에 추가해야 할 Navigator이다.
 import { createDrawerNavigator } from "react-navigation-drawer";
 
 import CategoriesScreen from "../screens/CatagoriesScreen";
@@ -17,7 +15,6 @@ import FavoritesScreen from "../screens/FavoritesScreen";
 import FiltersScreen from "../screens/FiltersScreen";
 import Colors from "../constants/Colors";
 
-// 여기에서 좀 오류가 있어서 수정을 해줬다.
 const defaultStackNavOptions = {
   headerStyle: {
     backgroundColor: Colors.primaryColor,
@@ -74,10 +71,27 @@ const tabScreenConfig = {
   },
 };
 
-// 흠 일단 이걸 또 추가시키긴 했다.
-const FiltersNavigator = createStackNavigator({
-  Fliters: FiltersScreen,
-});
+const FiltersNavigator = createStackNavigator(
+  {
+    Fliters: FiltersScreen,
+  },
+
+  {
+    // FiltersNavigator에서만 동작하는 navigationOptions를 달아줄 수 있다.
+    // 이것은 뭐 default 를 무시하거나, Filters에서만 동작하도록 하는 무언가겠찌.
+    // navigationOptions: {
+    // drawerLabel 이라는 것은 Main Navigator에서 목록의 이름을
+    // 바꾸는 것과 관련이 있다. 밑에서도 바꿀 수 있찌만, 뭐 여기서도 바꿀 수 있다.
+    // 그런말을 하는 듯 하다.
+    // 뭐 이건 적용은 안할거고 필요할떄 알아서 바꾸면 된다 이거다.
+    // 그런데 여기서 하기 보다는 그냥 MainNavigator에서 config를 하는 방법을
+    // 여기서는 선택했다.
+    // drawerLabel: "Filters!!",
+    // },
+    // FiltersNavigator에다가도 defaultNavigationOptions를 달아줬다.
+    defaultNavigationOptions: defaultStackNavOptions,
+  }
+);
 
 const MealsFavTabNavigator =
   Platform.OS === "android"
@@ -91,14 +105,32 @@ const MealsFavTabNavigator =
         },
       });
 
-// 여기다가 이번에 배울 Navigator를 config 해보자
-const MainNavigator = createDrawerNavigator({
-  // 또 이거를 중첩시키네.. 뭐 그럴 수 있찌.
-  MealsFavs: MealsFavTabNavigator,
-  // 얘도 방금 만든 것임.
-  Filters: FiltersNavigator,
-});
+const MainNavigator = createDrawerNavigator(
+  {
+    // 여기에 나오는 것들이
+    // MainNavigator에 나오는 이름이 되는 것임.
+    // 여기 나오는 Key 값들을 바꾸면은 그 이름이 바뀌게 되는 것임.
+    MealsFavs: {
+      // 이런식으로 조금 수정을 해서 MainNavigator의 목록의
+      // 이름을 Root Navigator를 통해서 수정을 했다
+      screen: MealsFavTabNavigator,
+      navigationOptions: {
+        drawerLabel: "Meals",
+      },
+    },
+    Filters: FiltersNavigator,
+  },
+  // 여기는 MainNavigator의 목록에 대해서 더 option을 줄 수 있다는 건데
+  // 여기서는 styling에 관한 것을 적용해보았다.
+  // 뭐 더 자세한 것은 당연히 docs에 가서 확인을 해봐야겠다.
+  {
+    contentOptions: {
+      activeTintColor: Colors.accentColor,
+      labelStyle: {
+        fontFamily: "open-sans",
+      },
+    },
+  }
+);
 
-// export default createAppContainer(MealsFavTabNavigator);
-// 이것도 이제 Root Navigator가 바뀌었으니까 바꿔줘야 한다.
 export default createAppContainer(MainNavigator);
--- screens/CatagoriesScreen.js
+++ screens/CatagoriesScreen.js
@@ -10,7 +10,6 @@ import { CATEGORIES } from "../data/dummy-data";
 import Colors from "../constants/Colors";
 import CategoryGridTile from "../components/CategoryGridTile";
 
-// 저번에 했던거 그대로 하는 것임.
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
 
@@ -51,10 +50,6 @@ CategoriesScreen.navigationOptions = (navData) => {
           title="Menu"
           iconName="ios-menu"
           onPress={() => {
-            // 누르면은 navigation이 toggle이 되서 들어갔다가 나왔다가
-            // 한다는 그런말이다. 여기서 navigation은 main navigation이니까
-            // Drawer Navigation을 뜻한다 뭐 그렇게 알고 있으면 된다.
-            // 이렇게 하면은 간단하게 햄버거메뉴를 만들 수 있다는 그런 말이다.
             navData.navigation.toggleDrawer();
           }}
         />
