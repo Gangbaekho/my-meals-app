54db29724ec2b921fd2a7950a86ad27fa2fe959c
kimjinsoo <roorooro9933@gmail.com>
2021-01-18 16:20:51 +0900

    dispatching actions and reducer logic

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,11 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
 import DefaultText from "../components/DefaultText";
 
 import { MEALS } from "../data/dummy-data";
-import { useSelector } from "react-redux";
+// Favorites Meals를 추가하기 위해서는 Dispatch가 필요하기 떄문에
+// useDispatch를 추가해주면 된다.
+import { useSelector, useDispatch } from "react-redux";
+// Dispatch를 사용한다는 것은 결국에는 action을 사용한다는 의미니까
+// 사용하고 싶은 action을 import 해줘야 한다.
+import { toggleFavorite } from "../store/actions/meals";
 
 const ListItem = (props) => {
   return (
@@ -22,9 +27,25 @@ const MealDetailScreen = (props) => {
 
   const selectedMeal = availableMeals.find((meal) => meal.id === mealId);
 
-  // useEffect(() => {
-  //   props.navigation.setParams({ mealTitle: selectedMeal.title });
-  // }, [selectedMeal]);
+  // 이런식으로 일단은 dispatch라는 것을 만들어준다음에
+  // 적절한 곳에 사용을 하면 된다.
+  // 한번 intiailize 해주는게 좋겠찌 매번 useDispatch를 호출하는 것 보다.
+  const dispatch = useDispatch();
+
+  // 여기서도 useCallback을 사용햇으니까
+  // 왜 사용했는지, depencency는 왜 dispatch와 mealId로 했는지에 대해서
+  // 생각을 해보면 된다.
+  const toggleFavoriteHandler = useCallback(() => {
+    dispatch(toggleFavorite(mealId));
+  }, [dispatch, mealId]);
+
+  // 이걸 하는 것은 headerRight에다가 해당 기능을 붙여넣어여 하기 때문에
+  // navigation에다가 해당 기능을 넣어주는 것으로 생각을 하면 된다.
+  // navigation에 있는 button을 이용하지 않을 것이면 굳이 안해도
+  // 되는 것으로 생각된다.
+  useEffect(() => {
+    props.navigation.setParams({ toggleFav: toggleFavoriteHandler });
+  }, [toggleFavoriteHandler]);
 
   return (
     <ScrollView>
@@ -48,8 +69,11 @@ const MealDetailScreen = (props) => {
 };
 
 MealDetailScreen.navigationOptions = (navigationData) => {
-  const mealId = navigationData.navigation.getParam("mealId");
+  // const mealId = navigationData.navigation.getParam("mealId");
   const mealTitle = navigationData.navigation.getParam("mealTitle");
+  // 위에서 넘겨준 것을 여기로 받은다음에, Icon을 클릭할떄
+  // 이것이 발동하게 만들어주면 된다는 것이지.
+  const toggleFavorite = navigationData.navigation.getParam("toggleFav");
   return {
     headerTitle: mealTitle,
     headerRight: (
@@ -57,9 +81,8 @@ MealDetailScreen.navigationOptions = (navigationData) => {
         <Item
           title="Favorite"
           iconName="ios-star"
-          onPress={() => {
-            console.log("Mark as favorite");
-          }}
+          // 여기에서 toggleFavorite를 적용한 모습이다.
+          onPress={toggleFavorite}
         />
       </HeaderButtons>
     ),
--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -1,4 +1,5 @@
 import { MEALS } from "../../data/dummy-data";
+import { TOGGLE_FAVORITE } from "../actions/meals";
 
 const initialState = {
   meals: MEALS,
@@ -7,6 +8,25 @@ const initialState = {
 };
 
 const mealsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case TOGGLE_FAVORITE:
+      const existingIdex = state.favoriteMeals.findIndex(
+        (meal) => meal.id === action.id
+      );
+      if (existingIdex >= 0) {
+        const updatedFavMeals = [...state.favoriteMeals];
+        updatedFavMeals.splice(existingIdex, 1);
+        // 여기서 주의해야 할 것은 ...state 해가지고 모든것을 일단 집어넣고
+        // favoriteMeals만 덮어씌운다는 그런 의미이다.
+        // 나머지는 뭐 비지니스 로직이기 때문에 신경쓸 필요는 없다.
+        return { ...state, favoriteMeals: updatedFavMeals };
+      } else {
+        const meal = state.meals.find((meal) => meal.id === action.id);
+        return { ...state, favoriteMeals: state.favoriteMeals.concat(meal) };
+      }
+    default:
+      return state;
+  }
   return state;
 };
 
